68e3479909d39c50f501f6bdf567cbdec423e765
Mhamad6000 <mhamadothman6000@gmail.com>
2022-10-26 23:05:37 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  if (someValue > 42) {
+    return someValue - 42;
+  } else {
+    return 42 - someValue;
+  }
+}
+function distanceFromHqInFeet(someValue) {
+  return distanceFromHqInBlocks(someValue) * 264;
+}
+function distanceTravelledInFeet(num1, num2) {
+  if (num2 > num1) {
+    return (num2 - num1) * 264;
+  } else {
+    return (num1 - num2) * 264;
+  }
+}
+function calculatesFarePrice(start, destination) {
+  if (distanceFromHqInFeet(destination) - distanceFromHqInFeet(start) <= 400) {
+    return 0;
+  } else if (
+    distanceFromHqInFeet(destination) - distanceFromHqInFeet(start) > 400 &&
+    distanceFromHqInFeet(destination) - distanceFromHqInFeet(start) < 2000
+  ) {
+    return (
+      (distanceFromHqInFeet(destination) - distanceFromHqInFeet(start) - 400) *
+      0.02
+    );
+  } else if (
+    distanceFromHqInFeet(destination) - distanceFromHqInFeet(start) > 2000 &&
+    distanceFromHqInFeet(destination) - distanceFromHqInFeet(start) < 2500
+  ) {
+    return 25;
+  } else if (
+    distanceFromHqInFeet(destination) - distanceFromHqInFeet(start) >
+    2500
+  ) {
+    return "cannot travel that far";
+  }
+}
